499305d47d054e332c72ab6334909c6d43c6556b
cizquierdo <carlos.izquierdof@gmail.com>
2019-10-14 13:35:25 +0000

    fixed missing quote

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -15,10 +15,10 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # Set up Jenkins with sufficient resources
 oc new-app jenkins-persistent \
 	--param ENABLE_OAUTH=true \
-	--param MEMORY_LIMIT=1Gi \
+	--param MEMORY_LIMIT=2Gi \
 	--param VOLUME_CAPACITY=5Gi \
 	--param DISABLE_ADMINISTRATIVE_MONITORS=true \
-#	--env JENKINS_JAVA_OVERRIDES="-Dhudson.slaves.NodeProvisioner.initialDelay=0 -Dhudson.slaves.NodeProvisioner.MARGIN=50 -Dhudson.slaves.NodeProvisioner.MARGIN0=0.85 -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=300" \
+	--env JENKINS_JAVA_OVERRIDES="-Dhudson.slaves.NodeProvisioner.initialDelay=0 -Dhudson.slaves.NodeProvisioner.MARGIN=50 -Dhudson.slaves.NodeProvisioner.MARGIN0=0.85 -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=300" \
 	-n ${GUID}-jenkins
 
 # Create custom agent container image with skopeo
